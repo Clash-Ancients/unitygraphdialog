14d2a0373593dccfb33f88dc7657fe8c745decf5
xiaoyulan <15230497577@163.com>
2023-12-21 10:12:30 +0800

    create entry node

--- Assets/Dialog/Editor/DialogGraphView.cs
+++ Assets/Dialog/Editor/DialogGraphView.cs
@@ -1,4 +1,7 @@
+using System;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class DialogGraphView : GraphView
@@ -8,5 +11,22 @@ public class DialogGraphView : GraphView
         this.AddManipulator(new ContentDragger());
         this.AddManipulator(new SelectionDragger());
         this.AddManipulator(new RectangleSelector());
+
+        AddElement(GenerateEntryPointNode());
+    }
+
+    DialogNode GenerateEntryPointNode()
+    {
+        var entryNode = new DialogNode
+        {
+            title = "Entry Node",
+            GUID = Guid.NewGuid().ToString(),
+            DialogText = "ENTRYPOINT",
+            EntryPoint = true,
+        };
+        
+        entryNode.SetPosition(new Rect(100f, 100f, 100f, 100f));
+
+        return entryNode;
     }
 }
