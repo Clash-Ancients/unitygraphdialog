3de3053a0a1f68b342ec3d3e1c35637f2dc554a6
xiaoyulan <15230497577@163.com>
2023-12-21 10:04:48 +0800

    add graphview into rootvisualelement

--- Assets/Dialog/Editor/DialogGraph.cs
+++ Assets/Dialog/Editor/DialogGraph.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DialogGraph : EditorWindow
 {
@@ -8,5 +10,24 @@ public class DialogGraph : EditorWindow
     {
         var win = GetWindow<DialogGraph>();
         win.titleContent = new GUIContent("Dialogue Graph");
+    }
+
+    private DialogGraphView _mDialogGraphView;
+    
+    private void OnEnable()
+    {
+        _mDialogGraphView = new DialogGraphView
+        {
+            name = "Dialog Graph",
+        };
+        
+        _mDialogGraphView.StretchToParentSize();
+        
+        rootVisualElement.Add(_mDialogGraphView);
+    }
+
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(_mDialogGraphView);
     }
 }
