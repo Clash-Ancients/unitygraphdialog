eaad1b3f4206fc9b62b959bc15d86f7b2ba52963
xiaoyulan <15230497577@163.com>
2023-12-21 16:05:11 +0800

    save graph

--- Assets/Dialog/Editor/GraphSaveUtility.cs
+++ Assets/Dialog/Editor/GraphSaveUtility.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GraphSaveUtility
 {
    private DialogGraphView mTargetGraphView;
 
+   private List<Edge> Edges => mTargetGraphView.edges.ToList();
+   private List<DialogNode> Nodes => mTargetGraphView.nodes.ToList().Cast<DialogNode>().ToList();
+   
+   
+   
    public static GraphSaveUtility GetInstance(DialogGraphView _graphView)
    {
       return new GraphSaveUtility
@@ -16,6 +24,43 @@ public class GraphSaveUtility
 
    public void SaveGraph(string fileName)
    {
+      if(!Edges.Any())
+         return;
+
+      var dialogContainer = ScriptableObject.CreateInstance<DialogueContainer>();
+
+      var connectedPorts = Edges.Where(x => x.input.node != null).ToList();
+
+      foreach (var VARIABLE in connectedPorts)
+      {
+         var outputNode = VARIABLE.output.node as DialogNode;
+         var inputNode = VARIABLE.input.node as DialogNode;
+          
+         dialogContainer.ListNodeLinkData.Add(new NodeLinkData
+         {
+            BaseNodeGuid = outputNode.GUID,
+            PortName = VARIABLE.output.portName,
+            TargetNodeGuid = inputNode.GUID,
+         });
+      }
+
+      foreach (var VARIABLE in Nodes.Where(node => !node.EntryPoint))
+      {
+         dialogContainer.ListDialogueNodeData.Add(new DialogueNodeData
+         {
+            GUID = VARIABLE.GUID,
+            DialogueText = VARIABLE.DialogText,
+            Position = VARIABLE.GetPosition().position,
+         });
+      }
+
+      if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+      {
+         AssetDatabase.CreateFolder("Assets", "Resources");
+      }
+      
+      AssetDatabase.CreateAsset(dialogContainer, $"Assets/Resources/{fileName}.asset");
+      AssetDatabase.SaveAssets();
       
    }
 
